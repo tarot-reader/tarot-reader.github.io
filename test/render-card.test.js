7631b86f7cc1548f8f38c3fcf42b2f1633645364
evanandrewson <evanandrewson@gmail.com>
2019-08-05 11:47:02 -0700

    added render card function and test

---
+++ test/render-card.test.js
@@ -0,0 +1,25 @@
+import renderCard from '../src/library/render-card.js';
+
+const test = QUnit.test;
+
+QUnit.module('Render library');
+
+test('Render one card', assert => {
+    // arrange
+    const toRender = {
+        name: 'The Fool',
+        number: 0,
+        description: 'Inexperience, one seeking fulfillment, foolishness.',
+        readingText: 'this is what would show up in the reading',
+        readingReversed: 'this is what shows up if the card is upside down',
+        imgSrc: 'assets/the-fool.webp',
+    };
+    const expected = '<div class="card"><img src="assets/the-fool.webp" alt="The Fool"><p>Inexperience, one seeking fulfillment, foolishness.</p></div>';
+    
+    // act
+    const rendered = renderCard(toRender);
+    const html = rendered.outerHTML;
+    
+    // assert
+    assert.equal(expected, html);
+});
