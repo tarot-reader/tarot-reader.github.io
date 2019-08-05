fbd57c1d286b20f5dbdb52e6e8d96915bf0bbbae
antonella <antonella.gr27@gmail.com>
2019-08-05 16:06:19 -0700

    fixing Travis

--- src/welcome/get-user-info.js
+++ src/welcome/get-user-info.js
@@ -1,17 +1,17 @@
 const form = document.getElementById('user-info');
 
-const userName = document.getElementById('name').value;
-const birthday = document.getElementById('birthday').value;
+// const userName = document.getElementById('name').value;
+// const birthday = document.getElementById('birthday').value;
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
     const formData = new FormData(form);
 
-    const info = {
-        name: formData.get('name'),
-        birthday: formData.get('birthday'),
-    };
+    // const info = {
+    //     name: formData.get('name'),
+    //     birthday: formData.get('birthday'),
+    // };
 
     form.reset();
 });
