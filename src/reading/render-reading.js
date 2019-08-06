3e01477584f283eade3a6f5fe9af5fd6466ca8af
Maeve Griffin <griffinmaeve@gmail.com>
2019-08-05 21:31:53 -0700

    fixed reading

--- src/reading/render-reading.js
+++ src/reading/render-reading.js
@@ -1,4 +1,4 @@
-// import info from '../data/data-manage.js';
+import info from '../data/data-manage.js';
 
 const renderReading = document.getElementById('render-reading');
 const middleCardFace = document.getElementById('middle-card-face');
@@ -8,10 +8,20 @@ const middleCardBack = document.getElementById('middle-card-back');
 const rightCardBack = document.getElementById('right-card-back');
 const leftCardBack = document.getElementById('left-card-back');
 
+const threeCardArray = info.randomCards(3);
+
+
 function showFaceCards() {
-    middleCardFace.classList.remove('hidden');
-    rightCardFace.classList.remove('hidden');
-    leftCardFace.classList.remove('hidden');
+
+    const image1 = threeCardArray[0].imgSrc;
+    const image2 = threeCardArray[1].imgSrc;
+    const image3 = threeCardArray[2].imgSrc;
+    
+    console.log(image1, image2, image3);
+    middleCardFace.src = image1;
+    rightCardFace.src = image2;
+    leftCardFace.src = image3;
+    
 }
 
 function hideBackCards() {
@@ -24,4 +34,5 @@ renderReading.addEventListener('click', event => {
     event.preventDefault();
     showFaceCards();
     hideBackCards();
+
 });
