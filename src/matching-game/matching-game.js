0f8e21e1010ee9f4d38fcbdcd32c53fd8981304d
evanandrewson <evanandrewson@gmail.com>
2019-08-07 15:55:51 -0700

    got match funcionality working

--- src/matching-game/matching-game.js
+++ src/matching-game/matching-game.js
@@ -10,8 +10,6 @@ const cardsDisplay = document.getElementById('cards-display');
 
 // get cards elements by class
 const cards = document.getElementsByClassName('card');
-const fronts = document.getElementsByClassName('front');
-const backs = document.getElementsByClassName('back');
 
 // duplicate array to make array of 16 cards
 const sixteenCardArray = [];
@@ -42,15 +40,47 @@ for(let i = 0; i < shuffledArray.length; i++){
 // on button click, reveal card and disable button. Add id to a revealed array.
 
 const cardArray = [...cards];
-console.log(cardArray);
+let openedCards = [];
+let openedButtons = [];
 for(let i = 0; i < cardArray.length; i++) {
-    cardArray[i].addEventListener('click', (event) => {
-        console.log(event.currentTarget);
+    cardArray[i].addEventListener('click', (event) => {;
         const button = event.currentTarget;
         button.childNodes[0].classList.add('hidden');
         button.childNodes[1].classList.remove('hidden');
+        openedCards.push(button.value);
+        openedButtons.push(button);
+        cardCheck();
     });
 }
 
+function cardCheck() {
+    if(openedCards.length < 2) {
+        return;
+    }
+
+    if(openedCards[0] === openedCards[1]) {
+        match();
+    } else {
+        unmatch();
+    }
+    openedCards = [];
+}
+function match() {
+    openedButtons[0].childNodes[1].classList.add('match');
+    openedButtons[1].childNodes[1].classList.add('match');
+    openedButtons[0].disabled = true;
+    openedButtons[1].disabled = true;
+    openedButtons = [];
+}
+function unmatch() {
+    setTimeout(() => {
+        openedButtons[0].childNodes[0].classList.remove('hidden');
+        openedButtons[0].childNodes[1].classList.add('hidden');
+        console.log(openedButtons[0].childNodes[0]);
+        openedButtons[1].childNodes[0].classList.remove('hidden');
+        openedButtons[1].childNodes[1].classList.add('hidden');
+        openedButtons = [];
+    }, 2000);
+}
 // on next button click, reveal card and check to see if it's a match. If it's a match, keep both cards revealed. If not, hide both cards again.
 // display win message with play again button 
--- src/matching-game/render-button.js
+++ src/matching-game/render-button.js
@@ -5,14 +5,10 @@ function renderButton(card, index) {
     button.classList.add('card');
     const backImage = document.createElement('img');
     backImage.src = '/assets/back.png';
-    backImage.alt = 'back' + index;
-    backImage.classList.add('back');
     button.appendChild(backImage);
     const image = document.createElement('img');
     image.src = card.imgSrc;
     image.classList.add('hidden');
-    image.classList.add('front');
-    image.alt = 'image' + index;
     button.appendChild(image);
     return button;
 }
