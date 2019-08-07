dbe9d62984e47cbb627279d9d6ee55c5beaac15b
sanvean74 <dlambertpdx@gmail.com>
2019-08-07 11:14:19 -0700

    pair program - got render cards working

--- src/matching-game/matching-game.js
+++ src/matching-game/matching-game.js
@@ -1,8 +1,12 @@
 import info from '../data/data-manage.js';
+import renderButton from './render-button.js';
 
 // generate array of 8 cards
 const eightCardArray = info.randomCards(8);
 console.log(eightCardArray);
+
+// get DOM element by ID
+const cardsDisplay = document.getElementById('cards-display');
 
 // duplicate array to make array of 16 cards
 const sixteenCardArray = [];
@@ -24,7 +28,14 @@ function shuffle(array) {
 }
 const shuffledArray = shuffle(sixteenCardArray);
 console.log(shuffledArray);
+
 // render cards as buttons
+for(let i = 0; i < shuffledArray.length; i++){
+    renderButton(shuffledArray[i], i);
+    const button = renderButton(shuffledArray[i], i);
+    cardsDisplay.appendChild(button);
+    
+}
 // on button click, reveal card and disable button. Add id to a revealed array.
 // on next button click, reveal card and check to see if it's a match. If it's a match, keep both cards revealed. If not, hide both cards again.
 // display win message with play again button 
--- src/matching-game/render-button.js
+++ src/matching-game/render-button.js
@@ -2,6 +2,7 @@ function renderButton(card, index) {
     const button = document.createElement('button');
     button.value = card.name;
     button.id = 'button' + index;
+    button.classList.add('hidden');
     const image = document.createElement('img');
     image.src = card.imgSrc;
     button.appendChild(image);
