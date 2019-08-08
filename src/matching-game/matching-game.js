2a07f685258281de364c5f0b096f1ecb9d3ae6be
evanandrewson <evanandrewson@gmail.com>
2019-08-07 17:16:15 -0700

    removed comments from matching-game.js

--- src/matching-game/matching-game.js
+++ src/matching-game/matching-game.js
@@ -1,17 +1,13 @@
 import info from '../data/data-manage.js';
 import renderButton from './render-button.js';
 
-// generate array of 8 cards
-const eightCardArray = info.randomCards(8);
 
-// get DOM element by ID
 const cardsDisplay = document.getElementById('cards-display');
 const winMessage = document.getElementById('win-message');
-// get cards elements by class
 const cards = document.getElementsByClassName('card');
 const resetButton = document.getElementById('reset-button');
 
-// duplicate array to make array of 16 cards
+const eightCardArray = info.randomCards(8);
 const sixteenCardArray = [];
 for(let i = 0; i < eightCardArray.length; i++) {
     sixteenCardArray.push(eightCardArray[i]);
@@ -31,13 +27,13 @@ function shuffle(array) {
 }
 const shuffledArray = shuffle(sixteenCardArray);
 
-// render cards as buttons
+
 for(let i = 0; i < shuffledArray.length; i++) {
     renderButton(shuffledArray[i], i);
     const button = renderButton(shuffledArray[i], i);
     cardsDisplay.appendChild(button);
 }
-// on button click, reveal card and disable button. Add id to a revealed array.
+
 
 const cardArray = [...cards];
 let openedCards = [];
@@ -66,7 +62,6 @@ function cardCheck() {
     }
     openedCards = [];
 }
-// on next button click, reveal card and check to see if it's a match. If it's a match, keep both cards revealed. If not, hide both cards again.
 function match() {
     totalMatches.push(openedButtons[0]);
     totalMatches.push(openedButtons[1]);
@@ -95,7 +90,6 @@ function unmatch() {
         }
     }, 2000);
 }
-// display win message with play again button 
 
 function displayWin() {
     for(let i = 0; i < cardArray.length; i++) {
